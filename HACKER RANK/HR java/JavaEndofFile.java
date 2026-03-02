1533334a45ea497994413808d3f056d9c263131a
AdityaGit121 <adityakunche121@gmail.com>
2026-03-02 12:04:06 +0530

    Add files via upload

---
+++ HACKER RANK/HR java/JavaEndofFile.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int count = 1;
+        try(Scanner in = new Scanner(System.in))
+        {
+            while(in.hasNext())
+            {
+                System.out.println(count + " " + in.nextLine());
+                ++count;
+            }
+        }
+    }
+}
